a40c6a196137b5dc92757bd18f415ac9a09a411b
Salis <m.salis@go-jek.com>
2016-12-02 00:08:32 +0700

    add binding adapters

--- app/src/main/java/com/bloonerd/mvvm_databinding/DetailViewModel.java
+++ app/src/main/java/com/bloonerd/mvvm_databinding/DetailViewModel.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailViewModel {
 
 
     private final GoTCharacter gotCharacter;
-    private Context context;
+    private final Context context;
 
     public DetailViewModel(GoTCharacter gotCharacter, Context context) {
         this.gotCharacter = gotCharacter;
@@ -39,5 +42,18 @@ public class DetailViewModel {
 
     public Drawable getImageHouse() {
         return ContextCompat.getDrawable(context, gotCharacter.houseResId);
+    }
+
+    public String getImageCharacter() {
+        return gotCharacter.fullUrl != null ? gotCharacter.fullUrl : "";
+    }
+
+    @BindingAdapter("app:imgUrl")
+    public static void setImgUrl(ImageView view, String url) {
+        Picasso.with(view.getContext())
+                .load(Uri.parse(url))
+                .placeholder(R.drawable.profile_placeholder_full)
+                .error(R.drawable.profile_placeholder_error_full)
+                .into(view);
     }
 }
